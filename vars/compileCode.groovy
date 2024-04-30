7da86e6283a5e6d84e7b432f32f187c904fb7726
nidakhan990 <164150254+nidakhan990@users.noreply.github.com>
2024-04-30 14:33:01 +0530

    Create compileCode.groovy

---
+++ vars/compileCode.groovy
@@ -0,0 +1,27 @@
+def call(String repoUrl, String branch) {
+    node {
+        try {
+            stage('Python Version') {
+                cleanWs() // Ensure a clean workspace before executing this stage
+                sh 'python3 --version'
+            }
+
+            stage('Git Clone') {
+                 checkout([$class: 'GitSCM', branches: [[name: "${branch}"]], userRemoteConfigs: [[url: "${repoUrl}"]]])
+
+            }
+
+            stage("Dast") {
+                sh ' wget https://github.com/zaproxy/zaproxy/releases/download/v2.14.0/ZAP_2.14.0_Linux.tar.gz'
+                sh ' tar -xf ZAP_2.14.0_Linux.tar.gz'
+                sh 'chmod 777 ZAP_2.14.0'
+                sh 'ZAP_2.14.0/zap.sh -cmd -port 8086 -quickurl http://3.21.100.62:8080/api/v1/attendance/health -quickprogress -quickout ${WORKSPACE}/out2.html'
+            }
+
+        } finally {
+            stage('Report') {
+            publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: '/var/lib/jenkins/workspace/dast/', reportFiles: 'out2.html', reportName: 'HTML Report', reportTitles: '', useWrapperFileDirectly: true])
+            }
+        }
+    }
+}
